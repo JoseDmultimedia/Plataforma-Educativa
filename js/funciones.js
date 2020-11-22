9987898e27f16c1048dcda77c4077e3140e6dbcd
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2020-11-21 20:53:14 -0500

    Actualizacion req cliente

--- js/funciones.js
+++ js/funciones.js
@@ -2,22 +2,38 @@ let a;
 
 function badAnswer(){
     document.getElementById("1").style.display="inline";
+    document.getElementById("btAct1").style.display="none";
+    document.getElementById("btAct2").style.display="none";
+    document.getElementById("btAct3").style.display="none";
+    //-------------------------------------------------------------------
+
+
 }
 
 function goodAnswer(){
     document.getElementById("2").style.display="inline";
+    document.getElementById("btAct1").style.display="none";
+    document.getElementById("btAct2").style.display="none";
+    document.getElementById("btAct3").style.display="none";
 }
   
+//----------------------------------------------------------------------------------------------------
 
 function badAnswer3(){
     document.getElementById("1").style.display="inline";
     a = 1;
-    return a;    
+    document.getElementById("btAct5").style.display="none";
+    document.getElementById("btAct6").style.display="none";
+    return a;  
+    
+
 }
 
 function goodAnswer3(){
     document.getElementById("2").style.display="inline";
     a = 2;
+    document.getElementById("btAct5").style.display="none";
+    document.getElementById("btAct6").style.display="none";
     return a;
 }
   
@@ -26,9 +42,13 @@ function badAnswer23(){
     document.getElementById("4").style.display="inline";
     if(a==1){
         document.getElementById("6").style.display="inline";
+        document.getElementById("btAct7").style.display="none";
+        document.getElementById("btAct8").style.display="none";
     }
     else{
         document.getElementById("6").style.display="inline";
+        document.getElementById("btAct7").style.display="none";
+        document.getElementById("btAct8").style.display="none";
     }
 }
 
@@ -36,49 +56,113 @@ function goodAnswer23(){
     document.getElementById("3").style.display="inline";
     if(a==2){
         document.getElementById("5").style.display="inline";
+        document.getElementById("btAct7").style.display="none";
+        document.getElementById("btAct8").style.display="none";
     }
     if(a==1){
         document.getElementById("7").style.display="inline";
+        document.getElementById("btAct7").style.display="none";
+        document.getElementById("btAct8").style.display="none";
     }
 }
 
+//-------------------------------------------------------------------------------
+
+function badAnswer1(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("bta").style.display="none";
+    document.getElementById("btb").style.display="none";
+}
+function badAnswer2(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("btc").style.display="none";
+    document.getElementById("btd").style.display="none";
+}
+function badAnswer3(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("bte").style.display="none";
+    document.getElementById("btf").style.display="none";
+}
+function badAnswer4(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("btg").style.display="none";
+    document.getElementById("bth").style.display="none";
+}
+function badAnswer5(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("bti").style.display="none";
+    document.getElementById("btj").style.display="none";
+}
+function badAnswer6(){
+    document.getElementById("1").style.display="inline";
+    document.getElementById("btk").style.display="none";
+    document.getElementById("btl").style.display="none";
+}
+
+
+
 function a1(){
     document.getElementById("e1").style.display="inline";
+    document.getElementById("bta").style.display="none";
+    document.getElementById("btb").style.display="none";
 }
 
 function b1(){
     document.getElementById("e2").style.display="inline";
+    document.getElementById("btc").style.display="none";
+    document.getElementById("btd").style.display="none";
 }
 
 function c1(){
     document.getElementById("e3").style.display="inline";
+    document.getElementById("bte").style.display="none";
+    document.getElementById("btf").style.display="none";
 }
 
 function d1(){
     document.getElementById("e4").style.display="inline";
+    document.getElementById("btg").style.display="none";
+    document.getElementById("bth").style.display="none";
 }
 
 function e1(){
     document.getElementById("e5").style.display="inline";
+    document.getElementById("bti").style.display="none";
+    document.getElementById("btj").style.display="none";
 }
 
 function f1(){
     document.getElementById("e6").style.display="inline";
+    document.getElementById("btk").style.display="none";
+    document.getElementById("btl").style.display="none";
 }
 
+//--------------------------------------------------------------------------
 
 function bad(){
     document.getElementById("3extra").style.display="inline";
+    document.getElementById("bt21").style.display="none";
+    document.getElementById("bt22").style.display="none";
+    document.getElementById("bt23").style.display="none";
 }
 
 function good(){
     document.getElementById("2extra").style.display="inline";
+    document.getElementById("bt21").style.display="none";
+    document.getElementById("bt22").style.display="none";
+    document.getElementById("bt23").style.display="none";
 }
 
 function bad2(){
     document.getElementById("5extra").style.display="inline";
+    document.getElementById("bt31").style.display="none";
+    document.getElementById("bt32").style.display="none";
+    document.getElementById("bt33").style.display="none";
 }
 
 function good2(){
     document.getElementById("4extra").style.display="inline";
+    document.getElementById("bt31").style.display="none";
+    document.getElementById("bt32").style.display="none";
+    document.getElementById("bt33").style.display="none";
 }
